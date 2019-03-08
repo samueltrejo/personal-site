2b4f73b8a81609dba080045a878ff906bdf653ef
Samuel Trejo <samuel.trejo@s.tcatnashville.edu>
2019-03-07 20:19:35 -0600

    added printtodom and createcard functionality and tested

--- main.js
+++ main.js
@@ -1,1 +1,78 @@
-console.log('personal bio site')
+const projects = [
+  {
+    title: "Cool Project 1",
+    screenshot: "https://picsum.photos/300/300/?random",
+    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed:
+      "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+  },
+  {
+    title: "Cool Project 2",
+    screenshot: "https://picsum.photos/300/300/?random",
+    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed:
+      "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+  },
+  {
+    title: "Cool Project 3",
+    screenshot: "https://picsum.photos/300/300/?random",
+    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed:
+      "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: false,
+    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+  },
+  {
+    title: "Cool Project 4",
+    screenshot: "https://picsum.photos/300/300/?random",
+    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed:
+      "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+  },
+  {
+    title: "Cool Project 5",
+    screenshot: "https://picsum.photos/300/300/?random",
+    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed:
+      "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+  }
+];
+
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML = textToPrint;
+};
+
+const createProjectCards = () => {
+  let domString = "";
+  for (let i = 0; i < projects.length; i++) {
+    if (projects[i].available) {
+      domString += `<div class="project-cards">`;
+      domString += `<h3>${projects[i].title}</h3>`;
+      domString += `<img src="${projects[i].screenshot}">`;
+      domString += `<p>${projects[i].technologiesUsed}</p>`;
+      domString += `<a href="${projects[i].githubUrl}">Project Link</a>`;
+      domString += `</div>`;
+    }
+    printToDom('projectsPage', domString);
+  }
+};
+
+const init = () => {
+    createProjectCards()
+};
+
+init()
