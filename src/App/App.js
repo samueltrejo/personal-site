a9725165c70ceb0a6e3cf28aa9765ebbc51e3332
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-06-22 13:39:45 -0500

    react setup, and added homepage compoenent

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Homepage from '../components/homepage';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Homepage />
+      </div>
+    );
+  }
+}
+
+export default App;
